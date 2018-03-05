8ae2ef224275193a439687507921141d6761e3ce
balakrishnan-adappt <balakrishnan.r@adappt.co>
2018-03-05 19:42:27 +0530

    updated selection of feeds

--- src/com/pages/test_execution.java
+++ src/com/pages/test_execution.java
@@ -1,12 +1,18 @@
 package com.pages;
 
 import java.awt.Toolkit;
-import java.util.concurrent.TimeUnit;
+import java.util.Iterator;
+import java.util.List;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,14 +22,36 @@ public class test_execution {
 	WebDriver driver;
 	
 	@Test
-	public void select_feeds() {
+	public void select_feeds() throws InterruptedException {
+		List<WebElement> title= driver.findElements(By.xpath("//div[starts-with(@id,'5a')]"));
+		Iterator<WebElement> header=title.iterator();
+		while(header.hasNext()) {
+			WebElement titles=header.next();
+			if(titles.getText().equals("Oil & Gas")) {
+				System.out.println("feed is found");
+				Thread.sleep(3000);
+			boolean feed=titles.isSelected();
+			if(feed==true) {
+				System.out.println("feed is already subscribed");
+			}else {
+//				JavascriptExecutor executor = (JavascriptExecutor)driver;
+//				executor.executeScript("arguments[0].click();", titles);
+//				titles.click();
 		
-	}
+				Actions action = new Actions(driver);
+				action.moveToElement(titles).click().perform();
+				Thread.sleep(3000);
+			}
+			}
+		}
+		
+		}
+	
 	
 	
 	
 	@BeforeTest
-	public void setup() {
+	public void setup() throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bala\\Downloads\\chromedriver.exe");
 		driver=new ChromeDriver();
@@ -38,8 +66,9 @@ public class test_execution {
 		System.out.println("username is entered");
 		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Welcome1!");
 		System.out.println("password is enetered");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		Thread.sleep(5000);
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
 		System.out.println("login successful");
 		
 	}
